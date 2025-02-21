f7414d8686712968665c8234a7b8b2d7bc8b905e
VonRiddarn <vonriddarn@gmail.com>
2025-02-21 13:06:24 +0100

    feat: added button for toggling visible password

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,17 +1,22 @@
 import React, { JSX } from "react";
 
 type LoginFormProps = {
-	type: string;
+	type: "text" | "password";
 	id: string;
 	title: string;
 	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const LoginField = ({ type, id, title }: LoginFormProps): JSX.Element => {
+	const [hideInput, setHideInput] = React.useState(type === "password" ? true : false);
+
 	return (
 		<>
 			<label htmlFor={id}>{title}</label>
-			<input type={type} id={id}></input>
+			<input type={hideInput ? "password" : "text"} id={id}></input>
+			{type === "password" && (
+				<button onClick={() => setHideInput(!hideInput)}>{hideInput ? "🙈" : "🙉"}</button>
+			)}
 		</>
 	);
 };
